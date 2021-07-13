9c1af6ed385ab92f95771cee02a03717629a00c0
“zhuangzhi” <zzz418172370@qq.com>
2021-07-13 22:03:52 +0800

    7/13

---
+++ leetcode/src/main/java/lc/L86.java
@@ -0,0 +1,45 @@
+package lc;
+
+/**
+ * Created by zz on 2021/7/13.
+ */
+public class L86 {
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode();
+        ListNode smallHead = small;
+        ListNode large = new ListNode();
+        ListNode largeHead = large;
+
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ leetcode/src/main/java/lc/L92.java
@@ -0,0 +1,18 @@
+package lc;
+
+/**
+ * Created by zz on 2021/7/13.
+ */
+public class L92 {
+
+//    public ListNode reverseBetween(ListNode head, int m, int n) {
+//    }
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    }
