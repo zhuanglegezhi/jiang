eb2265c3b60aee6238d66276505b60c734b872f6
“zhuangzhi” <zzz418172370@qq.com>
2021-07-20 21:25:23 +0800

    7/20

---
+++ leetcode/src/main/java/lc/L206.java
@@ -0,0 +1,37 @@
+package lc;
+
+/**
+ * Created by zz on 2021/7/20.
+ */
+public class L206 {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
--- leetcode/src/main/java/lc/L92.java
+++ leetcode/src/main/java/lc/L92.java
@@ -5,14 +5,42 @@ package lc;
  */
 public class L92 {
 
-//    public ListNode reverseBetween(ListNode head, int m, int n) {
-//    }
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode cur = head;
+        ListNode pre = dummy;
+        int count = 1;
+
+        while (count < n) {
+            if (count < m) {
+                pre = cur;
+                cur = cur.next;
+            } else {
+                ListNode next = cur.next;
+                cur.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+            }
+            count++;
+        }
+        return dummy.next;
+    }
 
     public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+}
