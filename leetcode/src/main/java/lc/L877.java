d2364fd77e337742650ff88d712cd28a4e81b90e
“zhuangzhi” <zzz418172370@qq.com>
2021-08-18 22:54:02 +0800

    8/18

---
+++ leetcode/src/main/java/lc/L877.java
@@ -0,0 +1,22 @@
+package lc;
+
+/**
+ * Created by zz on 2021/8/18.
+ */
+public class L877 {
+
+    public boolean stoneGame(int[] piles) {
+        int len = piles.length;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            for (int j = i + 1; j < len; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][len - 1] > 0;
+    }
+
+}
