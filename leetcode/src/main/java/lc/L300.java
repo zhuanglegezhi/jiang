c4a09ff5cf5411090ad18c74f5b8c5eabbeafbaf
“zhuangzhi” <zzz418172370@qq.com>
2021-11-13 11:57:36 +0800

    11/13

--- leetcode/src/main/java/lc/L300.java
+++ leetcode/src/main/java/lc/L300.java
@@ -6,9 +6,10 @@ package lc;
 public class L300 {
 
     public int lengthOfLIS(int[] nums) {
-        if (nums == null) return 0;
-        int dp[] = new int[nums.length];
+        if (nums == null || nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
         int max = 0;
+
         for (int i = 0; i < nums.length; i++) {
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
@@ -18,6 +19,7 @@ public class L300 {
             }
             max = Math.max(dp[i], max);
         }
+
         return max;
     }
 }
--- leetcode/src/main/java/lc/L322.java
+++ leetcode/src/main/java/lc/L322.java
@@ -11,7 +11,7 @@ public class L322 {
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
-        for (int i = 1; i < amount + 1; i++) {
+        for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
                 if (i - coin >= 0) {
                     dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
