d6ccf3e5dbd2657186c076e75ecd9d7aec9b6d24
“zhuangzhi” <zzz418172370@qq.com>
2021-11-21 10:58:36 +0800

    11/21

---
+++ leetcode/src/main/java/lc/L174.java
@@ -0,0 +1,51 @@
+package lc;
+
+import java.util.Arrays;
+
+/**
+ * Created by zz on 2021/11/21.
+ */
+public class L174 {
+
+    int calculateMinimumHP(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        // 备忘录中都初始化为 -1
+        dp = new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return dp(grid, 0, 0);
+    }
+
+    // 备忘录，消除重叠子问题
+    int[][] dp;
+
+    /* 定义：从 (i, j) 到达右下角，需要的初始血量至少是多少 */
+    int dp(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        // base case
+        if (i == m - 1 && j == n - 1) {
+            return grid[i][j] >= 0 ? 1 : -grid[i][j] + 1;
+        }
+        if (i == m || j == n) {
+            return Integer.MAX_VALUE;
+        }
+        // 避免重复计算
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        // 状态转移逻辑
+        int res = Math.min(
+                dp(grid, i, j + 1),
+                dp(grid, i + 1, j)
+        ) - grid[i][j];
+        // 骑士的生命值至少为 1
+        dp[i][j] = res <= 0 ? 1 : res;
+
+        return dp[i][j];
+    }
+
+}
