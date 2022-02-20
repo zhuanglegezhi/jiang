366c7543594409aa4a14ca37b805e5d8162963ff
“zhuangzhi” <zzz418172370@qq.com>
2022-02-20 20:12:50 +0800

    2/20

---
+++ algorithm/src/main/java/剑指/O30.java
@@ -0,0 +1,45 @@
+package 剑指;
+
+import java.util.Stack;
+
+/**
+ * Created by zz on 2022/2/20.
+ */
+public class O30 {
+
+    class MinStack {
+
+        private final Stack<Integer> stack;
+        private final Stack<Integer> minStack;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MinStack() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            if (minStack.isEmpty() || minStack.peek() >= x) {
+                minStack.push(x);
+            }
+        }
+
+        public void pop() {
+            if (stack.pop().equals(minStack.peek())) {
+                minStack.pop();
+            }
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int min() {
+            return minStack.peek();
+        }
+    }
+
+}
